3656bda52da34237caedfa9f38648587d7db790d
Leo <losoviz@gmail.com>
2019-12-16 17:48:58 +0800

    TypeResolver doesn't extend from Post

--- src/TypeResolvers/LocationPostTypeResolver.php
+++ src/TypeResolvers/LocationPostTypeResolver.php
@@ -2,10 +2,24 @@
 namespace PoP\LocationPosts\TypeResolvers;
 
 use PoP\Posts\TypeResolvers\PostTypeResolver;
+use PoP\Translation\Facades\TranslationAPIFacade;
 use PoP\LocationPosts\TypeDataLoaders\LocationPostTypeDataLoader;
 
 class LocationPostTypeResolver extends PostTypeResolver
 {
+    public const NAME = 'LocationPost';
+
+    public function getTypeName(): string
+    {
+        return self::NAME;
+    }
+
+    public function getSchemaTypeDescription(): ?string
+    {
+        $translationAPI = TranslationAPIFacade::getInstance();
+        return $translationAPI->__('A post which has locations', 'locationposts');
+    }
+
     public function getTypeDataLoaderClass(): string
     {
         return LocationPostTypeDataLoader::class;
